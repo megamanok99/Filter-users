94618fecad076572774600303a08f754483c3a62
alex <megamanok99@gmail.com>
2021-01-22 18:09:52 +0300

    1st

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import UsersList from './components/UsersList';
+
+import profile from './profile.json';
+import Profile from './components/profile';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
 
 function App() {
+  const [cards,setCards]=React.useState([]);
+ const [value,setValue]=React.useState('');
+ 
+const [page,Setpage]=React.useState(1);
+ const [isEnd,setEnd]=React.useState(false);
+ const [load,setLoad]=React.useState(false);
+
+ const url=`https://5c3755177820ff0014d92711.mockapi.io/users?page=${page}&limit=10`;
+
+
+ const getUsers=async()=>{
+  setLoad(true);
+   
+   const {data} =await  axios.get(url)
+   
+   if(data.length){
+    setCards( prevCards=>[...prevCards,...data]);
+   }else{
+     setEnd(true);
+   }
+   setLoad(false);
+ }
+  
+  
+
+  function updateUrl(){
+    console.log(page);
+    Setpage(page+1);
+    
+  }
+
+  
+React.useEffect(() => {
+  
+  getUsers();
+  
+},[page]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+     <div className="app">
+       <input type="text" placeholder="search" onChange={(e)=>setValue(e.target.value)}/>
+       {load && 
+       <center> <h3>Loading...</h3></center>
+      }
+       {!load && <UsersList users={cards} value={value} />}
+
+       {!isEnd && <button onClick={updateUrl}>next user</button>}
+
+     </div>
+
+     
+    
+     
+      
+      
+   
   );
 }
 
